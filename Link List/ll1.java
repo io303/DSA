58e0096e5ca633ba9899e7bc4e07c807eaecf034
io303 <srahul12768@gmail.com>
2025-10-01 16:05:34 +0530

    Update ll1.java

--- Link List/ll1.java
+++ Link List/ll1.java
@@ -13,12 +13,65 @@ public class ll1 {
         }
     }
 
+public void  insertLast(int val){
+    Node node =new Node(val);
+    Node temp=head;
+    while(temp.next!=null){
+        temp=temp.next;
+    }
+ temp.next=node;
+
+}
+
+public void insertParticular(int val,int index){
+    if(index==0){
+        insert(val);
+        return;
+    }
+    Node temp=head;
+    for(int i=1;i<index;i++){
+        temp=temp.next;
+    }
+    Node node =new Node(val,temp.next);
+    temp.next=node;
+
+}
+
+public int deletef(){
+    int value=head.val;
+    head=head.next;
+    return value;
+
+}
+public int deletel(){
+    Node temp=head;
+    if(head==null){
+        System.out.println(("not posiible"));
+        return -1;
+    }
+    if(head.next==null){
+        int value=head.val;
+        head=null;
+    return value;
+    }
+    while(temp.next.next!=null){
+        temp=temp.next;
+    }
+    int value=temp.next.val;
+        temp.next=null;
+        return value;
+}
+
+
+
+
     public void print(){
         Node temp=head;
         while(temp!=null){
             System.out.print(temp.val+"->");
             temp=temp.next;
         }
+        System.out.println("END");
         
     }
 
@@ -32,6 +85,13 @@ class Main{
         l.insert(5);
         l.insert(6);
         l.print();
+        // l.insertLast(7);
+        l.insertParticular(8, 1);
+        l.print();
+    //    System.out.println( l.deletef());
+    System.out.println(l.deletel());
+       l.print();
+
 
     }
 }
