dcf1f22fd36c5ac42e50afe10693148fa499148e
Rahul Singh <srahul12768@gmail.com>
2026-04-06 16:38:56 +0530

    Time: 1296 ms (6.00%) | Memory: 77.1 MB (15.90%) - LeetSync

--- Array/1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
+++ Array/1352-maximum-profit-in-job-scheduling/maximum-profit-in-job-scheduling.java
@@ -1,32 +1,27 @@
 class Solution {
     public int jobScheduling(int[] startTime, int[] endTime, int[] profit) {
- 
-		int len = startTime.length;
-		int[][] jobs = new int[len][3];
-		for (int i = 0; i < len; i++) {
-			jobs[i] = new int[]{startTime[i], endTime[i], profit[i]};
-		}
-		Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
-
-		return dfs(0, jobs, new Integer[len]);
-	}
-
-	private int dfs(int cur, int[][] jobs, Integer[] memo) {
-		if (cur == jobs.length) return 0;
-		if (memo[cur] != null) return memo[cur];
-
-		int inclProf = jobs[cur][2] + dfs(findNext(cur, jobs), jobs, memo);
-		int exclProf = dfs(cur + 1, jobs, memo);
-
-		return memo[cur] = Math.max(inclProf, exclProf);
-	}
-
-	private int findNext(int cur, int[][] jobs) {
-		for (int next = cur + 1; next < jobs.length; next++) {
-			if (jobs[next][0] >= jobs[cur][1]) {
-				return next;
-			}
-		}
-		return jobs.length;
-	}
+        int n=startTime.length;
+        int jobs[][]=new int[n][3];
+        for(int i=0;i<n;i++){
+            jobs[i]=new int[]{startTime[i],endTime[i],profit[i]};
+        }
+        Arrays.sort(jobs,(a,b)->a[0]-b[0]);
+        return help(0,new Integer[n],jobs);
     }
+    int help(int curr,Integer dp[],int jobs[][]){
+        if(curr==jobs.length) return 0;
+        if(dp[curr]!=null)return dp[curr];
+
+        int s=help(curr+1,dp,jobs);
+        int p=jobs[curr][2]+help(find(curr,jobs),dp,jobs);
+        return dp[curr]=Math.max(s,p);
+    }
+    int find(int curr,int jobs[][]){
+        for(int next=curr+1;next<jobs.length;next++){
+            if(jobs[next][0]>=jobs[curr][1]){
+                return next;
+            }
+        }
+        return jobs.length;
+    }
+}
