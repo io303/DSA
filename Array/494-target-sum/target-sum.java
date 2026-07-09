259a069eee6e09a9d70c071a8e5bd27b15542fe3
Rahul Singh <srahul12768@gmail.com>
2026-07-09 15:32:50 +0530

    Time: 635 ms (12.05%) | Memory: 43.1 MB (70.13%) - LeetSync

--- Array/494-target-sum/target-sum.java
+++ Array/494-target-sum/target-sum.java
@@ -1,44 +1,19 @@
-import java.util.*;
-
 class Solution {
-    int[][] dp;
-
+    int c=0;
     public int findTargetSumWays(int[] nums, int target) {
-        int n = nums.length;
-        int totalsum = 0;
+        help(nums,target,0,0);
+        return c;
 
-        for (int num : nums) totalsum += num;
-
-        // ✅ Correct feasibility check
-        if ((totalsum - target) < 0 || (totalsum - target) % 2 != 0)
-            return 0;
-
-        int sum = (totalsum - target) / 2;
-
-        dp = new int[n][sum + 1];
-        for (int[] row : dp) Arrays.fill(row, -1);
-
-        return help(n - 1, sum, nums);
     }
-
-    int help(int i, int t, int[] arr) {
-        if (t < 0) return 0;  // ✅ Important
-
-        if (i == 0) {
-            if (t == 0 && arr[0] == 0) return 2; // +0 and -0
-            if (t == 0 || arr[0] == t) return 1;
-            return 0;
+    void help(int nums[],int t,int sum,int i){
+        if(i==nums.length){
+            if(sum==t){
+                c++;
+            }
+            return;
         }
 
-        if (dp[i][t] != -1) return dp[i][t];
-
-        int nottake = help(i - 1, t, arr);
-        int take = 0;
-
-        if (arr[i] <= t)
-            take = help(i - 1, t - arr[i], arr);
-
-        return dp[i][t] = take + nottake;
+        help(nums,t,sum-nums[i],i+1);
+        help(nums,t,sum+nums[i],i+1);
     }
 }
-
