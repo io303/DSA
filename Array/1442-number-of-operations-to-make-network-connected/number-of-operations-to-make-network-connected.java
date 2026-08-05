1b40d582091664031ff3ff84f891b48185a30ee5
Rahul Singh <srahul12768@gmail.com>
2026-08-06 01:04:13 +0530

    Time: 4 ms (91.56%) | Memory: 66.1 MB (70.62%) - LeetSync

--- Array/1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
+++ Array/1442-number-of-operations-to-make-network-connected/number-of-operations-to-make-network-connected.java
@@ -1,30 +1,60 @@
 class Solution {
-    public int makeConnected(int n, int[][] connections) {
-        if(connections.length<n-1) return -1;
-        int parent[]=new int[n];
-        for(int i=0;i<n;i++){
-            parent[i]=i;
-        }
-        for(int e[]:connections){
-            union(e[0],e[1],parent);
-        }
-        int c=0;
-        for(int i=0;i<n;i++){
-            if(parent[i]==i) c++;
-        }
-        return c-1;
-        
-    }
-    void union(int a,int b,int parent[]){
-        int pa=findParent(a,parent);
-        int pb=findParent(b,parent);
 
-        if(pa!=pb){
-            parent[pb]=pa;
+    public int makeConnected(int n, int[][] connections) {
+
+        if (connections.length < n - 1) {
+            return -1;
+        }
+
+        int[] parent = new int[n];
+        int[] rank = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
+        }
+
+        for (int[] edge : connections) {
+            union(edge[0], edge[1], parent, rank);
+        }
+
+        int components = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (findParent(i, parent) == i) {
+                components++;
+            }
+        }
+
+        return components - 1;
+    }
+
+    void union(int a, int b, int[] parent, int[] rank) {
+
+        int pa = findParent(a, parent);
+        int pb = findParent(b, parent);
+
+        if (pa == pb) {
+            return;
+        }
+
+        if (rank[pa] < rank[pb]) {
+            parent[pa] = pb;
+        }
+        else if (rank[pb] < rank[pa]) {
+            parent[pb] = pa;
+        }
+        else {
+            parent[pb] = pa;
+            rank[pa]++;
         }
     }
-    int findParent(int n,int parent[]){
-        if(parent[n]==n) return n;
-        return parent[n]=findParent(parent[n],parent);
+
+    int findParent(int node, int[] parent) {
+
+        if (parent[node] == node) {
+            return node;
+        }
+
+        return parent[node] = findParent(parent[node], parent);
     }
 }
