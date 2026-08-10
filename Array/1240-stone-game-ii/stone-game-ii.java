59e75802d04f2e4a52907f2ef6e89b3396e1fdf4
Rahul Singh <srahul12768@gmail.com>
2026-08-10 15:46:09 +0530

    Time: 12 ms (35.84%) | Memory: 46.4 MB (29.85%) - LeetSync

--- Array/1240-stone-game-ii/stone-game-ii.java
+++ Array/1240-stone-game-ii/stone-game-ii.java
@@ -1,42 +1,41 @@
 class Solution {
     private int n;
-    private int[] suffix;
-    private int[][] dp;
-
-    private int solve(int i, int m) {
-        if (i == n) {
+    private int[][][] t = new int[2][101][101]; // t[2][n+1][n+1] ~ O(n^3)
+    
+    private int solveForAlice(int[] piles, int person, int i, int M) {
+        if (i >= n) {
             return 0;
         }
-
-        if (dp[i][m] != -1) {
-            return dp[i][m];
+        
+        if (t[person][i][M] != -1) {
+            return t[person][i][M];
         }
-
-        int best = 0;
-
-        for (int x = 1; x <= 2 * m && i + x <= n; x++) {
-            int nextM = Math.max(m, x);
-            int current = suffix[i] - solve(i + x, nextM);
-            best = Math.max(best, current);
+        
+        int result = (person == 1) ? -1 : Integer.MAX_VALUE;
+        int stones = 0;
+        
+        for (int x = 1; x <= Math.min(2 * M, n - i); x++) {
+            stones += piles[i + x - 1];
+            
+            if (person == 1) { // Alice
+                result = Math.max(result, stones + solveForAlice(piles, 0, i + x, Math.max(M, x)));
+            } else { // Bob
+                result = Math.min(result, solveForAlice(piles, 1, i + x, Math.max(M, x)));
+            }
         }
-
-        return dp[i][m] = best;
+        
+        return t[person][i][M] = result;
     }
-
+    
     public int stoneGameII(int[] piles) {
         n = piles.length;
-        suffix = new int[n + 1];
-
-        for (int i = n - 1; i >= 0; i--) {
-            suffix[i] = suffix[i + 1] + piles[i];
+        
+        for (int[][] arr2D : t) {
+            for (int[] arr1D : arr2D) {
+                Arrays.fill(arr1D, -1);
+            }
         }
-
-        dp = new int[n][n + 1];
-
-        for (int i = 0; i < n; i++) {
-            java.util.Arrays.fill(dp[i], -1);
-        }
-
-        return solve(0, 1);
+        
+        return solveForAlice(piles, 1, 0, 1);
     }
 }
