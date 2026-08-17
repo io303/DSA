d9d4a27bc564beab5967e5ab08e4a4650481b674
Rahul Singh <srahul12768@gmail.com>
2026-08-17 18:04:39 +0530

    Time: 101 ms (30.34%) | Memory: 48.4 MB (5.34%) - LeetSync

--- Array/5-longest-palindromic-substring/longest-palindromic-substring.java
+++ Array/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,59 +1,28 @@
-import java.util.Arrays;
-
-class Solution {
-
-    int[][] dp;
-
+public class Solution {
     public String longestPalindrome(String s) {
+        if (s.length() <= 1) {
+            return s;
+        }
 
-        int n = s.length();
-
-        dp = new int[n][n];
-
-        for (int[] row : dp)
-            Arrays.fill(row, -1);
-
-        int start = 0;
         int maxLen = 1;
+        int start = 0;
+        int end = 0;
+        boolean[][] dp = new boolean[s.length()][s.length()];
 
-        for (int i = 0; i < n; i++) {
-
-            for (int j = i; j < n; j++) {
-
-                if (isPalindrome(s, i, j)) {
-
-                    if (j - i + 1 > maxLen) {
-
-                        maxLen = j - i + 1;
-                        start = i;
+        for (int i = 0; i < s.length(); ++i) {
+            dp[i][i] = true;
+            for (int j = 0; j < i; ++j) {
+                if (s.charAt(j) == s.charAt(i) && (i - j <= 2 || dp[j + 1][i - 1])) {
+                    dp[j][i] = true;
+                    if (i - j + 1 > maxLen) {
+                        maxLen = i - j + 1;
+                        start = j;
+                        end = i;
                     }
                 }
             }
         }
 
-        return s.substring(start, start + maxLen);
+        return s.substring(start, end + 1);
     }
-
-    private boolean isPalindrome(String s, int i, int j) {
-
-        if (i >= j)
-            return true;
-
-        if (dp[i][j] != -1)
-            return dp[i][j] == 1;
-
-        if (s.charAt(i) != s.charAt(j)) {
-
-            dp[i][j] = 0;
-            return false;
-        }
-
-        boolean ans = isPalindrome(s, i + 1, j - 1);
-
-        dp[i][j] = ans ? 1 : 0;
-
-        return ans;
-    }
-
-   
 }
