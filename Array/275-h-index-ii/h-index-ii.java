55ba3da372757d0c584981a3d5d43df812ffc63b
Rahul Singh <srahul12768@gmail.com>
2026-08-18 17:58:39 +0530

    Time: 0 ms (100.00%) | Memory: 49.9 MB (94.63%) - LeetSync

--- Array/275-h-index-ii/h-index-ii.java
+++ Array/275-h-index-ii/h-index-ii.java
@@ -1,25 +1,20 @@
 class Solution {
-    
     public int hIndex(int[] citations) {
-        int l=0,h=citations.length;
-        int ans=0;
-        while(l<=h){
-            int mid=l+(h-l)/2;
-            if(help(mid,citations)){
-                ans=Math.max(ans,mid);
-                l=mid+1;
-            }else{
-                h=mid-1;
+        int n = citations.length;
+
+        int left = 0;
+        int right = n - 1;
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (citations[mid] >= n - mid) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
             }
         }
-        return ans;
-    }
-    boolean help(int k,int citations[]){
-        int c=0;
 
-        for(int a:citations){
-            if(a>=k)c++;
-        }
-        return c>=k;
+        return n - left;
     }
 }
