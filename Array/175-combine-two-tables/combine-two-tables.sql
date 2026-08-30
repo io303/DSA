840a764795d4053958608c662108e19be114c43f
Rahul Singh <srahul12768@gmail.com>
2026-08-30 14:19:24 +0530

    Time: 411 ms (82.04%) | Memory: 0B (100.00%) - LeetSync

---
+++ Array/175-combine-two-tables/combine-two-tables.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+select p.firstName as firstName ,p.lastName as lastName,a.city as city,a.state
+from Person as p left join  Address as a on p.personId =a.personId;
