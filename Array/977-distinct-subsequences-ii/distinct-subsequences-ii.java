4fce8737991b55591db2849a2289497d7dd9a942
Rahul Singh <srahul12768@gmail.com>
2026-09-07 10:35:01 +0530

    Time: 5 ms (54.61%) | Memory: 43.4 MB (41.84%) - LeetSync

---
+++ Array/977-distinct-subsequences-ii/distinct-subsequences-ii.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int distinctSubseqII(String S) {
+        int n = S.length(), M = (int)1e9 + 7;
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int[] count = new int[26];
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            int index = S.charAt(i) - 'a';
+            dp[i] += sum - count[index];
+            dp[i] = (dp[i] + M) % M;
+            sum = (sum + dp[i]) % M;
+            count[index] = (count[index] + dp[i]) % M;
+        }
+        return sum;
+    }
+}
+ 
