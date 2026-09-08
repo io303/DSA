0f70fc1cb58f67a3b2608d82ddeafa2763d33907
Rahul Singh <srahul12768@gmail.com>
2026-09-08 20:10:33 +0530

    Time: 1 ms (83.65%) | Memory: 42.5 MB (57.08%) - LeetSync

--- Array/4245-count-commas-in-range/count-commas-in-range.java
+++ Array/4245-count-commas-in-range/count-commas-in-range.java
@@ -1,12 +1,8 @@
 class Solution {
     public int countCommas(int n) {
-       String s=String.valueOf(n);
-       int num=s.length();
-       if(num<4)return 0;
-      
-        int p=1000;
-        int ans=n-p;
-        
-      return ans+1;
+        if(n<1000){
+            return 0;
+        }
+        return n - 999;
     }
 }
