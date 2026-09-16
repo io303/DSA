83b8a0dd1b8fa367b5a87e529b8600def4e47348
Rahul Singh <srahul12768@gmail.com>
2026-09-16 16:00:28 +0530

    Time: 1197 ms (6.21%) | Memory: 46.7 MB (7.55%) - LeetSync

--- Array/60-permutation-sequence/permutation-sequence.java
+++ Array/60-permutation-sequence/permutation-sequence.java
@@ -1,26 +1,31 @@
 class Solution {
+    int c=0;
+    String ans="";
     public String getPermutation(int n, int k) {
-        StringBuilder sb=new StringBuilder();
-        List<Integer> ls=new ArrayList<>();
-       for(int i=1;i<=n;i++){
-        ls.add(i);
-       }
-      int fact=1;
-       for(int i=2;i<n;i++){
-        fact*=i;
-       }
-       k--;
+        boolean seen[]=new boolean[n+1];
 
-       while(true){
-        int index=k/fact;
+        help(n,k,seen,"");
+        return ans;
+    }
+    void help(int n,int k,boolean seen[],String curr){
+        if(curr.length()==n){
+            c++;
+            if(c==k){
+                ans=curr;
+            }
+            return;
+        }
 
-        sb.append(ls.get(index));
-        ls.remove(index);
-     
-        if(ls.size()==0)break;
-        k=k%fact;
-        fact=fact/ls.size();
-       }
-       return sb.toString();
+        for(int i=1;i<=n;i++){
+            if(!seen[i]){
+                seen[i]=true;
+                help(n,k,seen,curr+i);
+                seen[i]=false;
+
+                if(!ans.equals("")){
+                    return;
+                }
+            }
+        }
     }
 }
